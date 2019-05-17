2fae2080bf326528c6f0636d8dfa322246cfa94d
Lucas Miguel <luscasmm@outlook.com>
2019-05-17 11:55:57 -0300

    testing parallax

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+// Website loaded
+// $(document).ready(function(){
+// 	$('.parallax').parallax();
+// });
+
+// Scrol event
+$(document).scroll(() => {
+	// Get the scroll value
+	var scroll = $(document).scrollTop();
+
+	// Scrolls the parallax slower than the document's body
+	$(".parallax").css({ "transform": "translateY(-" + scroll / 4 + "px)" });
+});
+
 // Add Lune modal
