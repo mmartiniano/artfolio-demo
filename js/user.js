e666f083fdd485bd0a6b2fbf073883b3172a55e5
Lucas Miguel <luscasmm@outlook.com>
2019-05-28 17:45:47 -0300

    ...

--- js/user.js
+++ js/user.js
@@ -1,18 +1,18 @@
+$(".gallery").html( () => {
+	var html = "";
+	for(var i = 1; i < 10; i++) {
+		html += '<div class="gallery-item col">';
+			html += '<img class="img-modal-trigger" src="img/af-artessauro-img-' + i + '.jpg">';
+		html += '</div>';
+	}
+	return html;
+});
+
 // Inicializate Lune modal
 $(document).ready( () => {
 
-	$(".gallery").html( () => {
-		var html = "";
-		for(var i = 1; i < 10; i++) {
-			html += '<div class="gallery-item col">';
-				html += '<img class="img-modal-trigger" src="img/af-artessauro-img-' + i + '.jpg">';
-			html += '</div>';
-		}
-		return html;
-	});
-
 	L.modal();
-	
+
 });
 
 const navbar = $(".navbar"); // Stores the navar refference
