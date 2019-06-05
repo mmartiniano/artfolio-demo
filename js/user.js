3ce6481ec394a0e972b56d94840d4d60ef1f0c99
Lucas Miguel <luscasmm@outlook.com>
2019-06-05 17:46:25 -0300

    ...

--- js/user.js
+++ js/user.js
@@ -6,38 +6,45 @@ $(document).ready( () => {
 });
 
 const navbar = $(".navbar"); // Stores the navar refference
-var navbarOffset = navbar.offset().top;
-const user = $(".user");
-const profile = $(".profile.large");
+var navbarOffset = navbar.offset().top; // Stores the navbar distance from top of the page
+const user = $(".user"); // Profile image and user's name wrapper refference
+const profile = $(".profile.large"); // Main profile image refference
 
-var scroll = $(document).scrollTop();
+var scroll = $(document).scrollTop(); // Get the scroll value
 
 // Document scroll event
 $(document).scroll(() => {
 	// Get the scroll value
 	scroll = $(document).scrollTop();
+
+	// Try to turn navbar into fixed
 	fixNavbar();
 });
 
+// Try to turn navbar into fixed
 fixNavbar();
 
+// Set the navbar position to fixed when it is scrolled up
 function fixNavbar() {
-	// Set the navbar position to fixed when the first section is scrolled up
+	
 	if(scroll >= navbarOffset) {
 		// Verifies if navbar isn't already fixed
 		if(!navbar.hasClass("fixed")) {
-			$(".header").css("marginBottom", "180px");
+			if($(".gallery").height() < viewHeight) // Prevent bug when the gallery is too small
+				$(".header").css("marginBottom", "180px");
 			navbar.addClass("fixed"); // If itsn't, turns it fixed
 		}
 	} else {
 		// Verifies if navbar is already fixed
 		if(navbar.hasClass("fixed")) {
-			$(".header").css("marginBottom", "0");
+			if($(".gallery").height() < viewHeight) // Prevent bug when the gallery is too small
+				$(".header").css("marginBottom", "0");
 			navbar.removeClass("fixed");
 			user.addClass("hide");
 		}
 	}
 
+	// Show hide user's profile image and name
 	if(scroll >= profile.offset().top + profile.height()) 
 		user.removeClass("hide");
 	else
